0da7a4dba3a23bef3fb859672edc4ee703cb357d
Diego Dukão <diegodukao@gmail.com>
2016-04-18 23:21:31 -0300

    screen bg changed to white

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import kivy
 kivy.require('1.9.1')
 
-from kivy.app import App
 from kivy.config import Config
-
-Config.set('graphics', 'width', '394')
+Config.set('graphics', 'width', '393')
 Config.set('graphics', 'height', '700')
+
+from kivy.core.window import Window
+Window.clearcolor = (1, 1, 1, 1)
+
+from kivy.app import App
 
 
 class NostraPlataApp(App):
