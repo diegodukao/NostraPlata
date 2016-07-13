50babec7f3ad5df8be69ec6a54b2f996ea21fff3
Diego Dukão <diegodukao@gmail.com>
2016-07-13 19:09:44 -0300

    adding and removing add_transaction_screen correctly

--- main.py
+++ main.py
@@ -10,19 +10,25 @@ Config.set('graphics', 'height', '700')
 
 from kivy.app import App
 from kivy.garden.androidtabs import AndroidTabsBase, AndroidTabs
+from kivy.properties import ObjectProperty
 from kivy.uix.actionbar import ActionBar
 from kivy.uix.boxlayout import BoxLayout
 
 
 class NostraRoot(BoxLayout):
+    main_screen = ObjectProperty()
+    add_transaction_screen = ObjectProperty()
 
     def show_main_screen(self):
-        self.clear_widgets()
-        self.add_widget(MainScreen())
+        self.remove_widget(self.add_transaction_screen)
+        self.add_widget(self.main_screen)
 
     def show_add_transaction_screen(self):
-        self.clear_widgets()
-        self.add_widget(AddTransactionScreen())
+        self.remove_widget(self.main_screen)
+
+        if not self.add_transaction_screen:
+            self.add_transaction_screen = AddTransactionScreen()
+        self.add_widget(self.add_transaction_screen)
 
 
 class MainScreen(AndroidTabs):
@@ -52,6 +58,7 @@ class NostraPlata(App):
 
     def on_start(self):
         self.root.main.dashboard.populate_listview()
+        self.root.main_screen = self.root.main
 
 if __name__ == "__main__":
     NostraPlata().run()
